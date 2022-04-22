c7e80882ce554f529127f93c9b1091197854cd4b
Riad <felihriad5@gmail.com>
2022-04-23 00:20:52 +0200

    csrf url fix

--- next.config.js
+++ next.config.js
@@ -2,5 +2,8 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['localhost','res.cloudinary.com']
+  },
+  cookies: {
+    domains: ['intense-badlands-27342.herokuapp.com']
   }
 }
